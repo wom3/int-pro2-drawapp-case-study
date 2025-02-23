ba43545c63fbdcbb40bd9ab263347edc9a668028
wom3 <wom2@student.london.ac.uk>
2025-02-23 07:26:45 +0500

    Init

---
+++ sketch.js
@@ -0,0 +1,59 @@
+//global variables that will store the toolbox colour palette
+//amnd the helper functions
+var toolbox = null;
+var colourP = null;
+var helpers = null;
+
+//spray can object literal
+sprayCan = {
+    name: "sprayCanTool",
+    icon: "assets/sprayCan.jpg",
+    points: 13,
+    spread: 10,
+    draw: function(){
+        //if the mouse is pressed paint on the canvas
+        //spread describes how far to spread the paint from the mouse pointer
+        //points holds how many pixels of paint for each mouse press.
+        if(mouseIsPressed){
+            for(var i = 0; i < this.points; i++){
+                point(random(mouseX-this.spread, mouseX + this.spread), 
+                    random(mouseY-this.spread, mouseY+this.spread));
+            }
+        }
+    }
+};
+
+function setup() {
+
+	//create a canvas to fill the content div from index.html
+	canvasContainer = select('#content');
+	var c = createCanvas(canvasContainer.size().width, canvasContainer.size().height);
+	c.parent("content");
+
+	//create helper functions and the colour palette
+	helpers = new HelperFunctions();
+	colourP = new ColourPalette();
+
+	//create a toolbox for storing the tools
+	toolbox = new Toolbox();
+
+	//add the tools to the toolbox.
+	toolbox.addTool(new FreehandTool());
+	toolbox.addTool(new LineToTool());
+	toolbox.addTool(sprayCan);
+	toolbox.addTool(new mirrorDrawTool());
+	background(255);
+
+}
+
+function draw() {
+	//call the draw function from the selected tool.
+	//hasOwnProperty is a javascript function that tests
+	//if an object contains a particular method or property
+	//if there isn't a draw method the app will alert the user
+	if (toolbox.selectedTool.hasOwnProperty("draw")) {
+		toolbox.selectedTool.draw();
+	} else {
+		alert("it doesn't look like your tool has a draw method!");
+	}
+}
